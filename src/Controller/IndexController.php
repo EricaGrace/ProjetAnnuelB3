8ce4c471ceb342b425790f3d754b0bc48f580767
Jérémie Mazard <mazardjeremie@gmail.com>
2022-06-22 16:45:55 +0200

    Reformat IndexController.php

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,47 +2,44 @@
 
 namespace App\Controller;
 
-use App\Entity\User;
-use App\Repository\UserRepository;
 use App\Routing\Attribute\Route;
-use DateTime;
 use Symfony\Component\HttpFoundation\Request;
 
 class IndexController extends AbstractController
 {
-  #[Route(path: "/", httpMethod: "GET")]
-  public function index(Request $request)
-  {
-    return $this->twig->render('index.html.twig', [
-        'request' => $request
-    ]);
-  }
+    #[Route(path: "/", httpMethod: "GET")]
+    public function index(Request $request)
+    {
+        return $this->twig->render('index.html.twig', [
+            'request' => $request
+        ]);
+    }
 
-  #[Route(path: "/contact", httpMethod: "GET", name: "contact")]
-  public function contactForm()
-  {
-    return $this->twig->render('index/contact.html.twig');
-  }
+    #[Route(path: "/contact", httpMethod: "GET", name: "contact")]
+    public function contactForm()
+    {
+        return $this->twig->render('index/contact.html.twig');
+    }
 
-  #[Route(path: "/contact", httpMethod: "POST", name: "handleContact")]
-  public function handleContactForm(Request $request)
-  {
-    $query = $request->request->all();
+    #[Route(path: "/contact", httpMethod: "POST", name: "handleContact")]
+    public function handleContactForm(Request $request)
+    {
+        $query = $request->request->all();
 
-    return $this->twig->render('index/contact.html.twig');
-  }
+        return $this->twig->render('index/contact.html.twig');
+    }
 
-  
-  #[Route(path: "/login", name: "login", httpMethod: "GET")]
-  public function login()
-  {
-    return $this->twig->render('connexion/login.html.twig');
-  }
 
-  #[Route(path: "/compte", name: "MonCompte", httpMethod: "GET")]
-  public function inscription()
-  {
-    return $this->twig->render('MonCompte/MonCompte.html.twig');
-  }
+    #[Route(path: "/login", httpMethod: "GET", name: "login")]
+    public function login()
+    {
+        return $this->twig->render('connexion/login.html.twig');
+    }
+
+    #[Route(path: "/compte", httpMethod: "GET", name: "account")]
+    public function inscription()
+    {
+        return $this->twig->render('MonCompte/MonCompte.html.twig');
+    }
 }
   
