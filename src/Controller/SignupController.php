6832223f11e1594ce419342d545d4c6efb164111
youssefloay <youssefloay@gmail.com>
2022-06-22 14:57:52 +0200

    work in progress

--- src/Controller/SignupController.php
+++ src/Controller/SignupController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Role;
 use App\Entity\User;
 use App\Repository\UserRepository;
 use App\Routing\Attribute\Route;
@@ -18,10 +19,8 @@ class SignupController extends AbstractController
     public function signUp (UserRepository $userRepository, Request $request )
     {
         $form = $request->request;
+
         
-        //TODO: remove once input has been added
-        $birthdate = new DateTime();
-       
         try {
             Validator::alpha('')->assert($form->get('last_name'));
             Validator::alpha('')->assert($form->get('first_name'));
@@ -29,7 +28,7 @@ class SignupController extends AbstractController
             Validator::alnum()->assert($form->get('password'));
             Validator::email()->assert($form->get('email'));
             Validator::Phone()->assert($form->get('phone'));
-            Validator::date()->assert($birthdate);
+            Validator::date('d/m/Y')->assert($form->get('birthdate'));
         } catch(NestedValidationException $exception) {
             print_r(
                 $exception->getMessages([
@@ -42,13 +41,17 @@ class SignupController extends AbstractController
 
         $user = new User();
 
+        $birthDate = DateTime::createFromFormat('d/m/Y', $form->get('birthdate'));
+
         $user->setName($form->get('last_name'))
             ->setFirstName($form->get('first_name'))
             ->setUsername($form->get('username'))
             ->setPassword($form->get('password'))
             ->setEmail($form->get('email'))
             ->setPhone($form->get('phone'))
-            ->setBirthDate($birthdate);
+            ->setRole((new Role())->setID(1)->setName('User'))
+            ->setCreatedAt(new DateTime())
+            ->setBirthDate($birthDate);
 
         $userRepository->save($user);
 
