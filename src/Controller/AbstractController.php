cc7faf284030a36d611a9e8009e7b127b385f265
Jérémie Mazard <mazardjeremie@gmail.com>
2022-06-23 20:46:41 +0200

    Methods return404(), render() and renderIf()

--- src/Controller/AbstractController.php
+++ src/Controller/AbstractController.php
@@ -2,14 +2,34 @@
 
 namespace App\Controller;
 
+use App\Routing\RouteNotFoundException;
 use Twig\Environment;
 
 abstract class AbstractController
 {
-  protected Environment $twig;
+    protected Environment $twig;
 
-  public function __construct(Environment $twig)
-  {
-    $this->twig = $twig;
-  }
+    public function __construct(Environment $twig)
+    {
+        $this->twig = $twig;
+    }
+
+    protected function renderIf($name, array $context = [], mixed ...$conditions)
+    {
+        foreach ($conditions as $condition) {
+            if (!$condition) $this->return404();
+        }
+
+        return $this->render($name, $context);
+    }
+
+    protected function return404()
+    {
+        throw new RouteNotFoundException();
+    }
+
+    protected function render($name, array $context = [])
+    {
+        return $this->twig->render($name, $context);
+    }
 }
