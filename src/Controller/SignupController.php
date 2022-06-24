69209a77e364ca4ada2d2331419d0097c99dc411
youssefloay <youssefloay@gmail.com>
2022-06-24 10:21:39 +0200

    v2 signup

--- src/Controller/SignupController.php
+++ src/Controller/SignupController.php
@@ -6,39 +6,39 @@ use App\Entity\Role;
 use App\Entity\User;
 use App\Repository\UserRepository;
 use App\Routing\Attribute\Route;
+use App\Routing\Router;
 use DateTime;
 use Respect\Validation\Validator;
 use Respect\Validation\Exceptions\NestedValidationException;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class SignupController extends AbstractController
 {
  
 
     #[Route(path: "/signup", name: "signup", httpMethod: 'POST')]
-    public function signUp (UserRepository $userRepository, Request $request )
+    public function signUp (UserRepository $userRepository, Request $request, Router $router)
     {
         $form = $request->request;
-
         
         try {
-            Validator::alpha('')->assert($form->get('last_name'));
-            Validator::alpha('')->assert($form->get('first_name'));
+            Validator::alpha()->assert($form->get('last_name'));
+            Validator::alpha()->assert($form->get('first_name'));
             Validator::alnum()->assert($form->get('username'));
             Validator::alnum()->assert($form->get('password'));
             Validator::email()->assert($form->get('email'));
             Validator::Phone()->assert($form->get('phone'));
             Validator::date('d/m/Y')->assert($form->get('birthdate'));
         } catch(NestedValidationException $exception) {
-            print_r(
-                $exception->getMessages([
-                    'alnum' => '{{name}} must contain only letters and digits',
-                    'noWhitespace' => '{{name}} cannot contain spaces',
-                    'length' => '{{name}}    must not have more than 15 chars',
-                ])
-            );
-        } 
 
+            return $this->render('User/inscription.html.twig',  [
+                'messages' => $exception->getMessages(),
+                'old' => $form
+            ]);
+
+        }       
+      
         $user = new User();
 
         $birthDate = DateTime::createFromFormat('d/m/Y', $form->get('birthdate'));
@@ -55,13 +55,13 @@ class SignupController extends AbstractController
 
         $userRepository->save($user);
 
-        echo "User saved"; 
+        return new RedirectResponse($router->getRouteUriFromName('login')); 
     }
 
     #[Route(path: "/signup", name: "signup", httpMethod: 'GET')]
     public function index ()
     {
-        echo $this->twig->render(
+        return $this->render(
             'user/inscription.html.twig',
         );
     }
